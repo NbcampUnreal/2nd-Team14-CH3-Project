997d7e8174c3528be6f11e4f30c9050d7c68ac42
oness68 <oness68@naver.com>
2025-02-25 20:29:40 +0900

    Merge branch 'UI/Menu' into Mergebranch

--- EscapeProtocol/Source/EscapeProtocol/Public/EPCharacter.h
+++ EscapeProtocol/Source/EscapeProtocol/Public/EPCharacter.h
@@ -20,45 +20,49 @@ public:
 	// Sets default values for this character's properties
 	AEPCharacter();
 	// 카메라 설정 관련
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
-	USpringArmComponent* SpringArmComp;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
-	UCameraComponent* CameraComp;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+    USpringArmComponent* SpringArmComp;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+    UCameraComponent* CameraComp;
 
-	FVector CameraSocketOffset = FVector(0.f, 40.f, 80.f);
+    FVector CameraSocketOffset = FVector(0.f, 40.f, 80.f);
 
-	// 캐릭터 무브먼트 관련
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
-	UCharacterMovementComponent* TPSMovementComp = GetCharacterMovement();
+    // 캐릭터 무브먼트 관련
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    UCharacterMovementComponent* TPSMovementComp = GetCharacterMovement();
 
-	// 캐릭터 Action 관련 
-	void Move(const FInputActionValue& Value);
-	void Look(const FInputActionValue& Value);
-	void StartJump(const FInputActionValue& Value);
-	void StopJump(const FInputActionValue& Value);
-	void StartCrouch(const FInputActionValue& Value);
-	void StopCrouch(const FInputActionValue& Value);
-	void StartSprint(const FInputActionValue& Value);
-	void StopSprint(const FInputActionValue& Value);
-	void Fire(const FInputActionValue& Value);
-	void Reload(const FInputActionValue& Value);
+    // 캐릭터 Action 관련 
+    void Move(const FInputActionValue& Value);
+    void Look(const FInputActionValue& Value);
+    void StartJump(const FInputActionValue& Value);
+    void StopJump(const FInputActionValue& Value);
+    void StartCrouch(const FInputActionValue& Value);
+    void StopCrouch(const FInputActionValue& Value);
+    void StartSprint(const FInputActionValue& Value);
+    void StopSprint(const FInputActionValue& Value);
+    void Fire(const FInputActionValue& Value);
+    void Reload(const FInputActionValue& Value);
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
-	float NormalGroundSpeed = 600.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
-	float SprintGroundSpeed = 1000.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    float NormalGroundSpeed = 600.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    float SprintGroundSpeed = 1000.0f;
 
-	bool bIsCrouching = false;
-	bool bIsJumping = false;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement|Sprint")
-	bool bIsSprinting = false;
-	
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement|Crouch")
+    bool bIsCrouching = false;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement|Jump")
+    bool bIsJumping = false;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement|Sprint")
+    bool bIsSprinting = false;
+
 protected:
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
 
 public:
-	virtual void Tick(float DeltaTime) override;
+    virtual void Tick(float DeltaTime) override;
 
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
 
 };
