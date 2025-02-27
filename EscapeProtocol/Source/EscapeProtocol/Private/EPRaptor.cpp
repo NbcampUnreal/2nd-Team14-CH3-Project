50d742952288e2e1f613282a33b176c853f5a81f
Xotepsin <167324082+Xotepsin@users.noreply.github.com>
2025-02-27 19:44:14 +0900

    update

--- EscapeProtocol/Source/EscapeProtocol/Private/EPRaptor.cpp
+++ EscapeProtocol/Source/EscapeProtocol/Private/EPRaptor.cpp
@@ -2,6 +2,14 @@
 
 AEPRaptor::AEPRaptor()
 {
-	PatrolSpeed = 300.0f;
+	PrimaryActorTick.bCanEverTick = false;
+
+
+	MaxHealth = 200.0f;
+	AttackDamage = 20.0f;
+	Health = MaxHealth;
+	patrolRadius = 1000.0f;
+	PatrolSpeed = 400.0f;
 	ChaseSpeed = 700.0f;
+
 }
