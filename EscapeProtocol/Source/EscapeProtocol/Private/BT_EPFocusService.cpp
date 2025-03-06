6b4b0477d0790b26c57a39ab4afbc766b9cb796d
Xotepsin <167324082+Xotepsin@users.noreply.github.com>
2025-03-06 19:04:49 +0900

    FocusService add

---
+++ EscapeProtocol/Source/EscapeProtocol/Private/BT_EPFocusService.cpp
@@ -0,0 +1,53 @@
+﻿#include "BT_EPFocusService.h"
+#include "EPAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBT_EPFocusService::UBT_EPFocusService()
+{
+	bNotifyBecomeRelevant = true;
+	bNotifyCeaseRelevant = true;
+	NodeName = TEXT("Focus on Target");
+
+}
+
+void UBT_EPFocusService::OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::OnBecomeRelevant(OwnerComp, NodeMemory);
+}
+
+void UBT_EPFocusService::OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::OnCeaseRelevant(OwnerComp, NodeMemory);
+
+	// 서비스가 끝날 때 Focus 해제
+	if (AEPAIController* AIController = Cast<AEPAIController>(OwnerComp.GetAIOwner()))
+	{
+		AIController->ClearFocus(EAIFocusPriority::Gameplay);
+	}
+}
+
+void UBT_EPFocusService::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	AEPAIController* AIController = Cast<AEPAIController>(OwnerComp.GetAIOwner());
+	if (!AIController) return;
+
+	// Blackboard에서 Actor 가져오기
+	FName KeyName = BlackboardKey.SelectedKeyName;
+	UObject* TargetObject = OwnerComp.GetBlackboardComponent()->GetValueAsObject(KeyName);
+	AActor* TargetActor = Cast<AActor>(TargetObject);
+
+	if (TargetActor)
+	{
+		AIController->SetFocus(TargetActor);
+	}
+	else
+	{
+		// 타겟이 없으면 Focus 해제
+		AIController->ClearFocus(EAIFocusPriority::Gameplay);
+	}
+
+}
+
+
---
+++ EscapeProtocol/Source/EscapeProtocol/Public/BT_EPFocusService.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "BT_EPFocusService.generated.h"
+
+UCLASS()
+class ESCAPEPROTOCOL_API UBT_EPFocusService : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+
+public:
+	UBT_EPFocusService();
+
+protected:
+    // 서비스가 활성화될 때(브랜치에 들어올 때) 한 번 호출
+    virtual void OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+    // 서비스가 비활성화될 때(브랜치에서 벗어날 때) 한 번 호출
+    virtual void OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+    // 매 프레임(틱)마다 실행
+    virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	
+};
